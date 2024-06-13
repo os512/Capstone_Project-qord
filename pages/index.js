d367be14170f53322c318055f62e041df7c798d0
Oliver Schupke <solanx@web.de>
2024-06-13 05:46:48 +0200

    Refactor: Include SignUpButton in home page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "@/styles/Home.module.css";
 import { M_PLUS_Rounded_1c } from "next/font/google";
 import NavBar from "../components/NavBar/NavBar.js";
+import SignUpButton from "../components/SignUpButton/SignUpButton.js";
 
 const mPlusRounded1cMainTitle = M_PLUS_Rounded_1c({
 	weight: "800",
@@ -26,14 +26,15 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<header className={`${styles.header}`}>
+			<header className={styles.header}>
 				<NavBar showAuthLinks={true} />
 			</header>
-			<main>
+			<main className={styles.main}>
 				<h1 className={mPlusRounded1cMainTitle.className}>qord: Your Melodic Compass </h1>
 				<h2 className={mPlusRounded1cSubTitle.className}>
 					Navigate Melodies with Just a Tonic and Mode Selection
 				</h2>
+				<SignUpButton>SignUp</SignUpButton>
 			</main>
 		</>
 	);
