738d64726e5cd43a8a1ad15fb89b3a5a3b0e8b80
Oliver Schupke <solanx@web.de>
2024-06-17 23:24:27 +0200

    Feat: Add getting-started page and implement frontend

---
+++ pages/getting-started.js
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import AuthButton from "@components/AuthButton/AuthButton.js";
+import { signOut, signIn, useSession } from "next-auth/react";
+import {
+	maintitle,
+	modes__container,
+	link__modes,
+	link__major,
+	link__minor,
+} from "@styles/GettingStarted.module.css";
+
+const GettingStarted = () => {
+	const { data: session } = useSession();
+
+	if (session) {
+		return (
+			<>
+				<h1 className={maintitle}>Choose a Mode</h1>
+				<div className={modes__container}>
+					<Link className={`${link__modes} ${link__major}`} href={"/getting-started"}>
+						Major
+					</Link>
+					<Link className={`${link__modes} ${link__minor}`} href={"/getting-started"}>
+						Minor
+					</Link>
+				</div>
+			</>
+		);
+	}
+
+	return (
+		<>
+			<h1 className={maintitle}>You are not logged in!</h1>
+			<AuthButton href="/">SignUp</AuthButton>
+		</>
+	);
+};
+
+export default GettingStarted;
