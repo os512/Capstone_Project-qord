846114ff1408d9f17406fac7463866959fb79abc
Oliver Schupke <solanx@web.de>
2024-06-17 23:23:24 +0200

    Refactor: Temporarily deactivate authentification via Github since it's not used in this project

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+// import GithubProvider from "next-auth/providers/github";
 import SpotifyProvider from "next-auth/providers/spotify";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "@lib/db/mongodb";
@@ -7,10 +7,10 @@ import clientPromise from "@lib/db/mongodb";
 export const authOptions = {
 	// Configure one or more authentication providers
 	providers: [
-		GithubProvider({
-			clientId: process.env.GITHUB_ID,
-			clientSecret: process.env.GITHUB_SECRET,
-		}),
+		// GithubProvider({
+		// 	clientId: process.env.GITHUB_ID,
+		// 	clientSecret: process.env.GITHUB_SECRET,
+		// }),
 		SpotifyProvider({
 			clientId: process.env.AUTH_SPOTIFY_ID,
 			clientSecret: process.env.AUTH_SPOTIFY_SECRET,
