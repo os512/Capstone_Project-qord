ba0dc168123a0955416a741f32385f822c0c020c
Oliver Schupke <solanx@web.de>
2024-06-18 02:17:32 +0200

    Refactor: Add functionality to customize the redirect url after successful login of the user

--- components/AuthButton/AuthButton.js
+++ components/AuthButton/AuthButton.js
@@ -1,9 +1,12 @@
 import { btn, btn_login } from "./AuthButton.module.css";
 import { signIn } from "next-auth/react";
 
-const AuthButton = () => {
+const AuthButton = ({ href }) => {
 	return (
-		<button className={`${btn} ${btn_login}`} onClick={() => signIn()}>
+		<button
+			className={`${btn} ${btn_login}`}
+			onClick={() => signIn(undefined, { callbackUrl: href })}
+		>
 			Sign in
 		</button>
 	);
