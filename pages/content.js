4565c06c5ef4f4f7b34ea5ea39135ba3e9d18c9f
Oliver Schupke <solanx@web.de>
2024-06-19 20:20:58 +0200

    Refactor: Add functionality to render the header and info text based on the user's choice relating mode and tonic

--- pages/content.js
+++ pages/content.js
@@ -1,16 +1,17 @@
 import { useRouter } from "next/router";
+import { signOut, signIn, useSession } from "next-auth/react";
 import useSWR from "swr";
+import AuthButton from "@components/AuthButton/AuthButton.js";
+import { maintitle, description } from "@styles/Content.module.css";
+
 
 const URL = "/scales-info.json";
 
 const fetcher = async (url) => {
 	const res = await fetch(url);
 
-	// If the status code is not in the range 200-299,
-	// we still try to parse and throw it.
 	if (!res.ok) {
 		const error = new Error("An error occurred while fetching the data.");
-		// Attach extra info to the error object.
 		error.info = await res.json();
 		error.status = res.status;
 		throw error;
@@ -19,27 +20,41 @@ const fetcher = async (url) => {
 };
 
 const Content = () => {
+	const { data: session } = useSession();
 	const router = useRouter();
 	const { mode, selectedScale } = router.query;
-
 	const parsedScale = typeof selectedScale === "string" ? JSON.parse(selectedScale) : selectedScale;
-	const tonic = parsedScale[0];
 
-	const { data, error, isLoading, mutate } = useSWR(URL, fetcher, {});
+	const { data, error, isLoading } = useSWR(URL, fetcher);
 
 	if (error) return <div>failed to load</div>;
 	if (isLoading) return <div>loading...</div>;
 
+	if (!mode || !selectedScale) {
+		router.push("/getting-started");
+		return;
+	}
+
+	const tonic = parsedScale[0];
 	const scaleInfo = data[mode]
 		.map((info) => info)
 		.find((scale) => scale.mode === mode && scale.tonic === tonic);
 
+
+	if (session) {
+		return (
+			<>
+				<h1 className={maintitle}>
+					The {tonic} {`${mode[0].toUpperCase()}${mode.slice(1)}`} Scale{" "}
+				</h1>
+				<p className={description}>{scaleInfo.description}</p>
+			</>
+		);
+	}
 	return (
 		<>
-			<h1>
-				The {tonic} {`${mode[0].toUpperCase()}${mode.slice(1)}`} Scale{" "}
-			</h1>
-			<p>{scaleInfo.description}</p>
+			<h1 className={maintitle}>You are not logged in!</h1>
+			<AuthButton href="/getting-started">SignUp</AuthButton>
 		</>
 	);
 };
