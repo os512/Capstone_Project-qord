fe0146ee8382dd7faf4854272bd2d5e1b817310b
Oliver Schupke <solanx@web.de>
2024-06-21 22:55:48 +0200

    Refactor: Remove unnecessary import

--- pages/content.js
+++ pages/content.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useEffect } from "react";
+import { useMemo, useEffect } from "react";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 
