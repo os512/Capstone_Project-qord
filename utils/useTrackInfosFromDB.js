dfab2a28d0c8905025e28661b560fcbe6caea2a8
Oliver Schupke <solanx@web.de>
2024-06-22 00:37:40 +0200

    Style: Remove leftover commented code

--- utils/useTrackInfosFromDB.js
+++ utils/useTrackInfosFromDB.js
@@ -1,26 +1,25 @@
 import useSWR from "swr";
 
 const fetcher = async (url) => {
-  const res = await fetch(url);
+	const res = await fetch(url);
 
-  if (!res.ok) {
-    const error = new Error("An error occurred while fetching the data.");
-    error.info = await res.json();
-    error.status = res.status;
-    throw error;
-  }
-  return res.json();
+	if (!res.ok) {
+		const error = new Error("An error occurred while fetching the data.");
+		error.info = await res.json();
+		error.status = res.status;
+		throw error;
+	}
+	return res.json();
 };
 
 const useTrackInfosFromDB = (mode, key) => {
-  const { data, error, isLoading } = useSWR(`/api/spotify/track?mode=${mode}&key=${key}`, fetcher);
-//   const { data, error, isLoading } = useSWR("/api/spotify/track?mode=Minor&key=F", fetcher);
+	const { data, error, isLoading } = useSWR(`/api/spotify/track?mode=${mode}&key=${key}`, fetcher);
 
-  return {
-    trackInfosFromDB: data,
-    isLoading,
-    isError: error,
-  };
+	return {
+		trackInfosFromDB: data,
+		isLoading,
+		isError: error,
+	};
 };
 
 export default useTrackInfosFromDB;
