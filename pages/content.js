26118a7c92e324feeded2e0070c3b4acbda645a4
Oliver Schupke <solanx@web.de>
2024-06-21 16:07:16 +0200

    Fix: The user is now redirected to the 'getting-start' page if he has accidentally changed the web browser address, as was previously the case

--- pages/content.js
+++ pages/content.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useEffect } from "react";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 import ContentPage from "@components/ContentPage/ContentPage";
@@ -12,84 +12,93 @@ import useNotePositions from "@utils/useNotePositions";
 import useTrackInfosFromDB from "@utils/useTrackInfosFromDB";
 
 const Content = () => {
-    const { data: session } = useSession();
-    const router = useRouter();
-    const { mode, selectedScale } = router.query;
+	const { data: session } = useSession();
+	const router = useRouter();
+	const { mode, selectedScale } = router.query;
 
-    const parsedScaleAndTonic = useMemo(() => {
-        if (!selectedScale) return { parsedScale: null, tonic: null };
-        try {
-            const parsedScale = typeof selectedScale === "string" ? JSON.parse(selectedScale) : selectedScale;
-            return { parsedScale, tonic: parsedScale[0] };
-        } catch (error) {
-            router.push("/getting-started");
-            return { parsedScale: null, tonic: null };
-        }
-    }, [selectedScale, router]);
+	const parsedScaleAndTonic = useMemo(() => {
+		if (!selectedScale) return { parsedScale: null, tonic: null };
+		try {
+			const parsedScale =
+				typeof selectedScale === "string" ? JSON.parse(selectedScale) : selectedScale;
+			return { parsedScale, tonic: parsedScale[0] };
+		} catch (error) {
+			router.push("/getting-started");
+			return { parsedScale: null, tonic: null };
+		}
+	}, [selectedScale, router]);
 
-    const { parsedScale, tonic } = parsedScaleAndTonic;
+	const { parsedScale, tonic } = parsedScaleAndTonic;
 
-    const capitalizedMode = mode ? mode.slice(0, 1).toUpperCase() + mode.slice(1) : '';
+	const capitalizedMode = mode ? mode.slice(0, 1).toUpperCase() + mode.slice(1) : "";
 
-    const { scaleInfo, isLoading: isScaleInfoLoading, isError: isScaleInfoError } = useScaleInfo();
-    const {
-        notePositions,
-        isLoading: isNotePositionsLoading,
-        isError: isNotePositionsError,
-    } = useNotePositions();
-    const {
-        trackInfosFromDB,
-        isLoading: isTrackInfosFromDBLoading,
-        isError: isTrackInfosFromDBError,
-    } = useTrackInfosFromDB(capitalizedMode, tonic ? encodeURIComponent(tonic) : '');
+	const { scaleInfo, isLoading: isScaleInfoLoading, isError: isScaleInfoError } = useScaleInfo();
+	const {
+		notePositions,
+		isLoading: isNotePositionsLoading,
+		isError: isNotePositionsError,
+	} = useNotePositions();
+	const {
+		trackInfosFromDB,
+		isLoading: isTrackInfosFromDBLoading,
+		isError: isTrackInfosFromDBError,
+	} = useTrackInfosFromDB(capitalizedMode, tonic ? encodeURIComponent(tonic) : "");
 
-    if (isScaleInfoError || isNotePositionsError || isTrackInfosFromDBError)
-        return <div>failed to load</div>;
-    if (isScaleInfoLoading || isNotePositionsLoading || isTrackInfosFromDBLoading)
-        return <div>loading...</div>;
+	useEffect(() => {
+		if (isScaleInfoError || isNotePositionsError || isTrackInfosFromDBError) {
+			router.push("/getting-started");
+		}
+	}, [isScaleInfoError, isNotePositionsError, isTrackInfosFromDBError, router]);
 
-    if (!mode || !parsedScale || !tonic) {
-        router.push("/getting-started");
-        return null;
-    }
+	useEffect(() => {
+		if (!mode || !parsedScale || !tonic) {
+			router.push("/getting-started");
+		}
+	}, [mode, parsedScale, tonic, router]);
 
-    console.log("trackInfosFromDB: ", trackInfosFromDB);
-    console.log("tonic: ", tonic);
-    
-    const scaleDetails = scaleInfo[mode].find((info) => info.mode === mode && info.tonic === tonic);
+	if (isScaleInfoLoading || isNotePositionsLoading || isTrackInfosFromDBLoading) {
+		return <div>loading...</div>;
+	}
 
-    const noteSystemCaption = `${scaleDetails["church-mode"][0].toUpperCase()}${scaleDetails["church-mode"].slice(1)}`;
+	console.log("trackInfosFromDB: ", trackInfosFromDB);
+	console.log("tonic: ", tonic);
 
-    const triads = notePositions.map((positions) =>
-        positions.map((position) => parsedScale[position])
-    );
+	const scaleDetails = scaleInfo[mode].find((info) => info.mode === mode && info.tonic === tonic);
 
-    const scaleInclOctaveDeclarations = prepareScale(parsedScale);
-    const triadsInclOctaveDeclarations = prepareTriad(triads);
+	const noteSystemCaption = `${scaleDetails["church-mode"][0].toUpperCase()}${scaleDetails[
+		"church-mode"
+	].slice(1)}`;
 
-    return (
-        <>
-            <ContentPage tonic={tonic} mode={mode} scaleInfo={scaleDetails} session={session} />
-            <div className={stave__container}>
-                <div className={stave__wrapper}>
-                    <ScaleNoteSystem scaleInclOctaveDeclarations={scaleInclOctaveDeclarations} />
-                    <p>
-                        {tonic} {mode} scale / {noteSystemCaption} mode
-                    </p>
-                </div>
-                <div className={stave__wrapper}>
-                    <TriadNoteSystem
-                        triadsInclOctaveDeclarations={triadsInclOctaveDeclarations}
-                        mode={mode}
-                        parsedScale={parsedScale}
-                    />
-                    <p>
-                        {tonic} {mode} triad | root position, 1st & 2nd inversion
-                    </p>
-                </div>
-            </div>
-        </>
-    );
+	const triads = notePositions.map((positions) =>
+		positions.map((position) => parsedScale[position])
+	);
+
+	const scaleInclOctaveDeclarations = prepareScale(parsedScale);
+	const triadsInclOctaveDeclarations = prepareTriad(triads);
+
+	return (
+		<>
+			<ContentPage tonic={tonic} mode={mode} scaleInfo={scaleDetails} session={session} />
+			<div className={stave__container}>
+				<div className={stave__wrapper}>
+					<ScaleNoteSystem scaleInclOctaveDeclarations={scaleInclOctaveDeclarations} />
+					<p>
+						{tonic} {mode} scale / {noteSystemCaption} mode
+					</p>
+				</div>
+				<div className={stave__wrapper}>
+					<TriadNoteSystem
+						triadsInclOctaveDeclarations={triadsInclOctaveDeclarations}
+						mode={mode}
+						parsedScale={parsedScale}
+					/>
+					<p>
+						{tonic} {mode} triad | root position, 1st & 2nd inversion
+					</p>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Content;
