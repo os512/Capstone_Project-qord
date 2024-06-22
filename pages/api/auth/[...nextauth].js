bc19b90435fd5cfca8bac8d71ef4dbe5fb14d29e
Oliver Schupke <solanx@web.de>
2024-06-22 10:06:36 +0200

    Fix: Now the authentication does work again

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,46 +1,45 @@
 import NextAuth from "next-auth";
-// import GithubProvider from "next-auth/providers/github";
 import SpotifyProvider from "next-auth/providers/spotify";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "@lib/db/mongodb";
 
 export const authOptions = {
-	// Configure one or more authentication providers
-	providers: [
-		// GithubProvider({
-		// 	clientId: process.env.GITHUB_ID,
-		// 	clientSecret: process.env.GITHUB_SECRET,
-		// }),
-		SpotifyProvider({
-			clientId: process.env.AUTH_SPOTIFY_ID,
-			clientSecret: process.env.AUTH_SPOTIFY_SECRET,
-			allowDangerousEmailAccountLinking: true, // TODO OS: This looks dirty!
-		}),
-	],
-	debug: true,
+  providers: [
+    SpotifyProvider({
+      clientId: process.env.AUTH_SPOTIFY_ID,
+      clientSecret: process.env.AUTH_SPOTIFY_SECRET,
+      authorization: {
+        params: {
+          scope: "streaming user-read-email user-read-private user-library-read user-library-modify user-read-playback-state user-modify-playback-state",
+        },
+      },
+      allowDangerousEmailAccountLinking: true,
+    }),
+  ],
+  debug: true,
+  adapter: MongoDBAdapter(clientPromise),
+  session: {
+    strategy: "database",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+    updateAge: 24 * 60 * 60, // 24 hours
+  },
+  callbacks: {
+    async session({ session, user }) {
+      // Fetch the user's account from the database
+      const client = await clientPromise;
+      const db = client.db();
+      const userAccount = await db.collection("accounts").findOne({ userId: user.id });
 
-	adapter: MongoDBAdapter(clientPromise),
-	callbacks: {
-		async signIn({ user, account, profile }) {
-			if (account.provider === "spotify") {
-				// You can augment user data here if necessary
-			}
-			return true;
-		},
-		async jwt({ token, user }) {
-			if (user) {
-				token.id = user.id;
-			}
-			return token;
-		},
-		async session({ session, user }) {
-			session.user.userId = user.id;
-			return session;
-		},
+      if (userAccount) {
+        session.accessToken = userAccount.access_token;
+        session.refreshToken = userAccount.refresh_token;
+        session.accessTokenExpires = userAccount.expires_at * 1000;
+      }
 
-	},
+      session.user.id = user.id;
+      return session;
+    },
+  },
 };
-
-console.log("session.accessToken: ", authOptions.callbacks.session.accessToken);
 
 export default NextAuth(authOptions);
